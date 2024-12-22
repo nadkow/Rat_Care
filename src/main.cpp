30d9a3ac39adf55d7f8e0f81549c4d48c62c47e2
nadkow <242435@edu.p.lodz.pl>
2024-12-22 01:05:27 +0100

    Moved gui code to separate file

--- src/main.cpp
+++ src/main.cpp
@@ -14,6 +14,10 @@
 #include <glm/gtc/type_ptr.hpp>
 #include <glm/gtx/string_cast.hpp>
 
+const char *glsl_version = "#version 460";
+constexpr int32_t GL_VERSION_MAJOR = 4;
+constexpr int32_t GL_VERSION_MINOR = 6;
+
 #include <ctime>
 #include <filesystem>
 #include <fstream>
@@ -24,6 +28,7 @@
 #include "logic/RatManager.h"
 #include "logic/TimeManager.h"
 #include "rendering/Skybox.h"
+#include "rendering/GuiManager.h"
 
 #ifndef STB_IMAGE_IMPLEMENTATION
 #define STB_IMAGE_IMPLEMENTATION
@@ -32,44 +37,11 @@
 
 /** FUNCTIONS **/
 
-static void glfw_error_callback(int error, const char *description) {
-    fprintf(stderr, "Glfw Error %d: %s\n", error, description);
-}
-
-void key_callback(GLFWwindow *window, int key, int scancode, int action, int mods);
-
-bool init();
-
-void init_imgui();
-
-void input();
-
 void update();
 
 void render();
 
-void imgui_begin();
-
-void imgui_render();
-
-void imgui_end();
-
-void end_frame();
-
 /** GLOBAL VARIABLES **/
-
-constexpr int32_t WINDOW_WIDTH = 1920;
-constexpr int32_t WINDOW_HEIGHT = 1080;
-
-GLFWwindow *window = nullptr;
-
-const char *glsl_version = "#version 460";
-constexpr int32_t GL_VERSION_MAJOR = 4;
-constexpr int32_t GL_VERSION_MINOR = 6;
-
-bool show_demo_window = true;
-bool show_another_window = false;
-ImVec4 clear_color = ImVec4(0.45f, 0.25f, 0.20f, 1.f);
 
 glm::vec3 cameraPos = glm::vec3(0.0f, 3.f, 10.0f);
 glm::vec3 cameraFront = glm::vec3(0.0f, -3.0f, -10.0f);
@@ -77,9 +49,7 @@ glm::vec3 cameraUp = glm::vec3(0.0f, 0.996f, 0.08f);
 glm::mat4 view = glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp);
 glm::mat4 projection = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f);
 
-bool firstMouse = true;
-uint8_t cursorMode = 1;
-int cursorModes[2] = {GLFW_CURSOR_DISABLED, GLFW_CURSOR_NORMAL};
+ImVec4 clear_color = ImVec4(0.45f, 0.25f, 0.20f, 1.f);
 
 float deltaTime = 0.0f;  // Time between current frame and last frame
 float lastFrame = 0.0f; // Time of last frame
@@ -90,20 +60,9 @@ RatManager ratManager(cage);
 
 int main(int, char **) {
 
-    if (!init()) {
-        spdlog::error("Failed to initialize project!");
-        return EXIT_FAILURE;
-    }
-    spdlog::info("Initialized project.");
-
-    init_imgui();
-    spdlog::info("Initialized ImGui.");
-
+    if (gui::init()) return EXIT_FAILURE;;
     dtm::init();
     init_skybox(dtm::hour);
-
-    glfwSetInputMode(window, GLFW_CURSOR, cursorModes[1]);
-    glfwSetKeyCallback(window, key_callback);
 
     glEnable(GL_DEPTH_TEST);
     glEnable(GL_CULL_FACE);
@@ -116,13 +75,13 @@ int main(int, char **) {
     ratManager.loadRats();
 
     // Main loop
-    while (!glfwWindowShouldClose(window)) {
+    while (!glfwWindowShouldClose(gui::window)) {
         currentFrame = glfwGetTime();
         deltaTime = currentFrame - lastFrame;
         lastFrame = currentFrame;
 
         // Process I/O operations here
-        input();
+        gui::input();
 
         // Update game objects' state here
         //update();
@@ -130,111 +89,14 @@ int main(int, char **) {
         // OpenGL rendering code here
         render();
 
-        // Draw ImGui
-        imgui_begin();
-        imgui_render();
-        imgui_end(); // this call effectively renders ImGui
-
-        // End frame and swap buffers (double buffering)
-        end_frame();
+        gui::draw();
     }
 
     // Save game
     ratManager.saveRats();
-
-    // Cleanup
-    ImGui_ImplOpenGL3_Shutdown();
-    ImGui_ImplGlfw_Shutdown();
-    ImGui::DestroyContext();
-
-    glfwDestroyWindow(window);
-    glfwTerminate();
+    gui::terminate();
 
     return 0;
-}
-
-bool init() {
-    // Setup window
-    glfwSetErrorCallback(glfw_error_callback);
-    if (!glfwInit()) {
-        spdlog::error("Failed to initalize GLFW!");
-        return false;
-    }
-
-    // GL 4.6 + GLSL 460
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, GL_VERSION_MAJOR);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, GL_VERSION_MINOR);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  // 3.2+ only
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);            // 3.0+ only
-
-    // Create window with graphics context
-    window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Dear ImGui GLFW+OpenGL4 example", NULL, NULL);
-    if (window == NULL) {
-        spdlog::error("Failed to create GLFW Window!");
-        return false;
-    }
-
-    glfwMakeContextCurrent(window);
-    glfwSwapInterval(1); // Enable VSync - fixes FPS at the refresh rate of your screen
-
-    bool err = !gladLoadGLLoader((GLADloadproc) glfwGetProcAddress);
-
-    if (err) {
-        spdlog::error("Failed to initialize OpenGL loader!");
-        return false;
-    }
-
-    return true;
-}
-
-void init_imgui() {
-    // Setup Dear ImGui binding
-    IMGUI_CHECKVERSION();
-    ImGui::CreateContext();
-    ImGuiIO &io = ImGui::GetIO();
-    (void) io;
-    //io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;  // Enable Keyboard Controls
-    //io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;   // Enable Gamepad Controls
-
-    ImGui_ImplGlfw_InitForOpenGL(window, true);
-    ImGui_ImplOpenGL3_Init(glsl_version);
-
-    // Setup style
-    ImGui::StyleColorsDark();
-    //ImGui::StyleColorsClassic();
-
-    // Load Fonts
-    // - If no fonts are loaded, dear imgui will use the default font. You can also load multiple fonts and use ImGui::PushFont()/PopFont() to select them.
-    // - AddFontFromFileTTF() will return the ImFont* so you can store it if you need to select the font among multiple.
-    // - If the file cannot be loaded, the function will return NULL. Please handle those errors in your application (e.g. use an assertion, or display an error and quit).
-    // - The fonts will be rasterized at a given size (w/ oversampling) and stored into a texture when calling ImFontAtlas::Build()/GetTexDataAsXXXX(), which ImGui_ImplXXXX_NewFrame below will call.
-    // - Read 'misc/fonts/README.txt' for more instructions and details.
-    // - Remember that in C/C++ if you want to include a backslash \ in a string literal you need to write a double backslash \\ !
-    //io.Fonts->AddFontDefault();
-    //io.Fonts->AddFontFromFileTTF("../../misc/fonts/Roboto-Medium.ttf", 16.0f);
-    //io.Fonts->AddFontFromFileTTF("../../misc/fonts/Cousine-Regular.ttf", 15.0f);
-    //io.Fonts->AddFontFromFileTTF("../../misc/fonts/DroidSans.ttf", 16.0f);
-    //io.Fonts->AddFontFromFileTTF("../../misc/fonts/ProggyTiny.ttf", 10.0f);
-    //ImFont* font = io.Fonts->AddFontFromFileTTF("c:\\Windows\\Fonts\\ArialUni.ttf", 18.0f, NULL, io.Fonts->GetGlyphRangesJapanese());
-    //IM_ASSERT(font != NULL);
-}
-
-void key_callback(GLFWwindow *window, int key, int scancode, int action, int mods) {
-    if (key == GLFW_KEY_R && action == GLFW_PRESS) {
-        ratManager.createRat("peepee");
-    }
-}
-
-void input() {
-    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-        glfwSetWindowShouldClose(window, true);
-
-    // rotate scene
-    if (glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_PRESS) {
-        ratManager.rotateRootLeft();
-    } else if (glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS) {
-        ratManager.rotateRootRight();
-    }
 }
 
 void update() {
@@ -249,47 +111,5 @@ void render() {
     draw_skybox();
     // draw all rats and cage
     ratManager.draw();
-}
-
-void imgui_begin() {
-    // Start the Dear ImGui frame
-    ImGui_ImplOpenGL3_NewFrame();
-    ImGui_ImplGlfw_NewFrame();
-    ImGui::NewFrame();
-}
-
-void imgui_render() {
-
-        ImGui::Begin("Wallet");
-
-        ImGui::Text("points = %d", 100);
-
-        ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate,
-                    ImGui::GetIO().Framerate);
-        ImGui::End();
-}
-
-void imgui_end() {
-    ImGui::Render();
-    int display_w, display_h;
-    glfwMakeContextCurrent(window);
-    glfwGetFramebufferSize(window, &display_w, &display_h);
-
-    glViewport(0, 0, display_w, display_h);
-
-    projection = glm::perspective(glm::radians(45.0f), (float) display_w / display_h, 0.1f, 100.0f);
-
-    ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
-}
-
-void end_frame() {
-    // Poll and handle events (inputs, window resize, etc.)
-    // You can read the io.WantCaptureMouse, io.WantCaptureKeyboard flags to tell if dear imgui wants to use your inputs.
-    // - When io.WantCaptureMouse is true, do not dispatch mouse input data to your main application.
-    // - When io.WantCaptureKeyboard is true, do not dispatch keyboard input data to your main application.
-    // Generally you may always pass all inputs to dear imgui, and hide them from your application based on those two flags.
-    glfwPollEvents();
-    glfwMakeContextCurrent(window);
-    glfwSwapBuffers(window);
 }
 
---
+++ src/rendering/GuiManager.h
@@ -0,0 +1,184 @@
+#ifndef RATCARE_GUIMANAGER_H
+#define RATCARE_GUIMANAGER_H
+
+extern RatManager ratManager;
+
+namespace gui {
+
+    constexpr int32_t WINDOW_WIDTH = 1920;
+    constexpr int32_t WINDOW_HEIGHT = 1080;
+
+    GLFWwindow *window = nullptr;
+
+    bool firstMouse = true;
+    uint8_t cursorMode = 1;
+    int cursorModes[2] = {GLFW_CURSOR_DISABLED, GLFW_CURSOR_NORMAL};
+
+    static void glfw_error_callback(int error, const char *description) {
+
+        fprintf(stderr, "Glfw Error %d: %s\n", error, description);
+
+    }
+
+    void key_callback(GLFWwindow *window, int key, int scancode, int action, int mods) {
+        if (key == GLFW_KEY_R && action == GLFW_PRESS) {
+            ratManager.createRat("peepee");
+        }
+    }
+
+    bool init_glfw() {
+        // Setup window
+        glfwSetErrorCallback(glfw_error_callback);
+        if (!glfwInit()) {
+            spdlog::error("Failed to initalize GLFW!");
+            return false;
+        }
+
+        // GL 4.6 + GLSL 460
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, GL_VERSION_MAJOR);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, GL_VERSION_MINOR);
+        glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  // 3.2+ only
+        glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);            // 3.0+ only
+
+        // Create window with graphics context
+        window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Dear ImGui GLFW+OpenGL4 example", NULL, NULL);
+        if (window == NULL) {
+            spdlog::error("Failed to create GLFW Window!");
+            return false;
+        }
+
+        glfwMakeContextCurrent(window);
+        glfwSwapInterval(1); // Enable VSync - fixes FPS at the refresh rate of your screen
+
+        bool err = !gladLoadGLLoader((GLADloadproc) glfwGetProcAddress);
+
+        if (err) {
+            spdlog::error("Failed to initialize OpenGL loader!");
+            return false;
+        }
+
+        return true;
+    }
+
+    void init_imgui() {
+        // Setup Dear ImGui binding
+        IMGUI_CHECKVERSION();
+        ImGui::CreateContext();
+        ImGuiIO &io = ImGui::GetIO();
+        (void) io;
+        //io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;  // Enable Keyboard Controls
+        //io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;   // Enable Gamepad Controls
+
+        ImGui_ImplGlfw_InitForOpenGL(window, true);
+        ImGui_ImplOpenGL3_Init(glsl_version);
+
+        // Setup style
+        ImGui::StyleColorsDark();
+        //ImGui::StyleColorsClassic();
+
+        // Load Fonts
+        // - If no fonts are loaded, dear imgui will use the default font. You can also load multiple fonts and use ImGui::PushFont()/PopFont() to select them.
+        // - AddFontFromFileTTF() will return the ImFont* so you can store it if you need to select the font among multiple.
+        // - If the file cannot be loaded, the function will return NULL. Please handle those errors in your application (e.g. use an assertion, or display an error and quit).
+        // - The fonts will be rasterized at a given size (w/ oversampling) and stored into a texture when calling ImFontAtlas::Build()/GetTexDataAsXXXX(), which ImGui_ImplXXXX_NewFrame below will call.
+        // - Read 'misc/fonts/README.txt' for more instructions and details.
+        // - Remember that in C/C++ if you want to include a backslash \ in a string literal you need to write a double backslash \\ !
+        //io.Fonts->AddFontDefault();
+        //io.Fonts->AddFontFromFileTTF("../../misc/fonts/Roboto-Medium.ttf", 16.0f);
+        //io.Fonts->AddFontFromFileTTF("../../misc/fonts/Cousine-Regular.ttf", 15.0f);
+        //io.Fonts->AddFontFromFileTTF("../../misc/fonts/DroidSans.ttf", 16.0f);
+        //io.Fonts->AddFontFromFileTTF("../../misc/fonts/ProggyTiny.ttf", 10.0f);
+        //ImFont* font = io.Fonts->AddFontFromFileTTF("c:\\Windows\\Fonts\\ArialUni.ttf", 18.0f, NULL, io.Fonts->GetGlyphRangesJapanese());
+        //IM_ASSERT(font != NULL);
+    }
+
+    int init() {
+        if (!init_glfw()) {
+            spdlog::error("Failed to initialize project!");
+            return 1;
+        }
+        spdlog::info("Initialized project.");
+
+        init_imgui();
+        spdlog::info("Initialized ImGui.");
+
+        glfwSetInputMode(window, GLFW_CURSOR, cursorModes[1]);
+        glfwSetKeyCallback(window, key_callback);
+        return 0;
+    }
+
+    void imgui_begin() {
+        // Start the Dear ImGui frame
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+    }
+
+    void imgui_render() {
+
+        ImGui::Begin("Wallet");
+
+        ImGui::Text("points = %d", 100);
+
+        ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate,
+                    ImGui::GetIO().Framerate);
+        ImGui::End();
+    }
+
+    void imgui_end() {
+        ImGui::Render();
+        int display_w, display_h;
+        glfwMakeContextCurrent(window);
+        glfwGetFramebufferSize(window, &display_w, &display_h);
+
+        glViewport(0, 0, display_w, display_h);
+
+        projection = glm::perspective(glm::radians(45.0f), (float) display_w / display_h, 0.1f, 100.0f);
+
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+    }
+
+    void end_frame() {
+        // Poll and handle events (inputs, window resize, etc.)
+        // You can read the io.WantCaptureMouse, io.WantCaptureKeyboard flags to tell if dear imgui wants to use your inputs.
+        // - When io.WantCaptureMouse is true, do not dispatch mouse input data to your main application.
+        // - When io.WantCaptureKeyboard is true, do not dispatch keyboard input data to your main application.
+        // Generally you may always pass all inputs to dear imgui, and hide them from your application based on those two flags.
+        glfwPollEvents();
+        glfwMakeContextCurrent(window);
+        glfwSwapBuffers(window);
+    }
+
+    void draw() {
+        // Draw ImGui
+        imgui_begin();
+        imgui_render();
+        imgui_end(); // this call effectively renders ImGui
+
+        // End frame and swap buffers (double buffering)
+        end_frame();
+    }
+
+    void terminate() {
+        // Cleanup
+        ImGui_ImplOpenGL3_Shutdown();
+        ImGui_ImplGlfw_Shutdown();
+        ImGui::DestroyContext();
+
+        glfwDestroyWindow(window);
+        glfwTerminate();
+    }
+
+    void input() {
+        if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+            glfwSetWindowShouldClose(window, true);
+        // rotate scene
+        if (glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_PRESS) {
+            ratManager.rotateRootLeft();
+        } else if (glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS) {
+            ratManager.rotateRootRight();
+        }
+    }
+}
+
+#endif //RATCARE_GUIMANAGER_H
